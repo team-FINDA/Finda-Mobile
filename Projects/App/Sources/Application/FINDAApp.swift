886d1cf1f5afe3be2961f2e42fd2b49ed10ce9b2
circle0802 <hawon9781@gmail.com>
2026-01-15 11:29:10 +0900

    ⚙️ :: (#5) Splash 화면 연결

--- Projects/App/Sources/Application/FINDAApp.swift
+++ Projects/App/Sources/Application/FINDAApp.swift
@@ -4,9 +4,7 @@ import SwiftUI
 struct FINDAApp: App {
     var body: some Scene {
         WindowGroup {
-            VStack {
-                Text("Hello, FINDA!")
-            }
+            ContentView()
         }
     }
 }
