bc21de5e2995d269387ac91165e15b6013a8e7ae
circle0802 <hawon9781@gmail.com>
2026-01-21 14:40:47 +0900

    ✨ :: (#7) 유도 버튼 AuthPromptButton Component 구현

---
+++ Projects/DesignSystem/Sources/Button/AuthPrompt/AuthPromptButton.swift
@@ -0,0 +1,40 @@
+import SwiftUI
+
+public struct AuthPromptButton: View {
+    let promptText: String
+    let buttonText: String
+    let action: () -> Void
+
+    public init(
+        promptText: String,
+        buttonText: String,
+        action: @escaping () -> Void = {}
+    ) {
+        self.promptText = promptText
+        self.buttonText = buttonText
+        self.action = action
+    }
+
+    public var body: some View {
+        HStack(spacing: 4) {
+            Text(promptText)
+                .font(.finda(.body4))
+                .foregroundStyle(DesignSystemAsset.Gray.gray60.swiftUIColor)
+            Button(
+                action: action
+            ) {
+                Text(buttonText)
+                    .font(.finda(.body3))
+                    .foregroundStyle(DesignSystemAsset.Gray.gray80.swiftUIColor)
+            }
+        }
+    }
+}
+
+#Preview {
+    AuthPromptButton(
+        promptText: "계정이 없으신가요?",
+        buttonText: "회원가입",
+        action: {}
+    )
+}
