24cd225f2af6b474117f2153e6f6c7758fe970ef
circle0802 <hawon9781@gmail.com>
2026-01-23 14:09:35 +0900

    ✨ :: (#7) SigninUserSelectView Publishing

---
+++ Projects/Features/FeatureAuth/Sources/Signin/SigninUserSelectView.swift
@@ -0,0 +1,38 @@
+import SwiftUI
+import DesignSystem
+
+struct SigninUserSelectView: View {
+    var body: some View {
+        VStack(spacing: 64) {
+            Text("로그인")
+                .font(.finda(.heading4))
+                .foregroundStyle(Color.Gray.gray80)
+                .padding(.top, 64)
+
+            VStack(spacing: 40) {
+                UserSelectButton(
+                    icon: Image.Images.pencil,
+                    text: "학생 로그인",
+                    action: {}
+                )
+                UserSelectButton(
+                    icon: Image.Images.book,
+                    text: "선생님 로그인",
+                    action: {}
+                )
+                AuthPromptButton(
+                    promptText: "계정이 없으신가요?",
+                    buttonText: "회원가입",
+                    action: {}
+                )
+            }
+            .padding(.horizontal, 24)
+
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    SigninUserSelectView()
+}
