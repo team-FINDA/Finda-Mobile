ea0ae561471d386ebc5e7d35acd7ae506dd8bd48
circle0802 <hawon9781@gmail.com>
2026-02-04 17:29:44 +0900

    ✨ :: (#7) 화면 터치 시 키보드 내리기

--- Projects/Features/FeatureAuth/Sources/Signin/SigninView.swift
+++ Projects/Features/FeatureAuth/Sources/Signin/SigninView.swift
@@ -62,6 +62,7 @@ struct SigninView: View {
 
             Spacer()
         }
+        .dismissKeyboardOnTap()
         .navigationBarBackButtonHidden(true)
         .toolbar(.hidden, for: .navigationBar)
     }
