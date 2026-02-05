fd9e039f94bc0a85c3cf0f525f5fb44c64720a25
circle0802 <hawon9781@gmail.com>
2026-02-05 19:10:56 +0900

    ✨ :: (#7) SecretKeyFeature 구현

---
+++ Projects/Features/FeatureAuth/Sources/Signup/SecretKey/SecretKeyFeature.swift
@@ -0,0 +1,32 @@
+import ComposableArchitecture
+
+@Reducer
+public struct SecretKeyFeature {
+    @ObservableState
+    public struct State: Equatable {
+        var secretKeyText = ""
+        var signupButtonIsDisable = true
+
+        public init() {}
+    }
+
+    public enum Action: BindableAction {
+        case binding(BindingAction<State>)
+        case nextButtonTapped
+    }
+
+    public init() {}
+
+    public var body: some ReducerOf<Self> {
+        BindingReducer()
+        Reduce { state, action in
+            switch action {
+            case .binding:
+                state.signupButtonIsDisable = state.secretKeyText.isEmpty
+                return .none
+            case .nextButtonTapped:
+                return .none
+            }
+        }
+    }
+}
--- Projects/Features/FeatureAuth/Sources/Signup/SecretKey/SecretKeyView.swift
+++ Projects/Features/FeatureAuth/Sources/Signup/SecretKey/SecretKeyView.swift
@@ -1,67 +1,66 @@
 import SwiftUI
+import ComposableArchitecture
 import DesignSystem
 
-struct SecretKeyView: View {
+public struct SecretKeyView: View {
     @Environment(\.dismiss) private var dismiss
-    @Binding var secretKeyText: String
-    var signupButtonIsDisable: Bool = true
+    @Perception.Bindable var store: StoreOf<SecretKeyFeature>
 
-    init(
-        secretKeyText: Binding<String>,
-        signupButtonIsDisable: Bool
-    ) {
-        self._secretKeyText = secretKeyText
-        self.signupButtonIsDisable = signupButtonIsDisable
+    public init(store: StoreOf<SecretKeyFeature>) {
+        self.store = store
     }
 
-    var body: some View {
-        VStack {
-            HStack {
-                Button(action: { dismiss() }, label: {
-                    Image.Icons.leftArrow
-                        .foregroundStyle(Color.Gray.gray80)
-                })
+    public var body: some View {
+        WithPerceptionTracking {
+            VStack {
+                HStack {
+                    Button(action: { dismiss() }, label: {
+                        Image.Icons.leftArrow
+                            .foregroundStyle(Color.Gray.gray80)
+                    })
+                    Spacer()
+                }
+                .padding(.horizontal, 24)
+                .padding(.vertical, 8)
+
+                Text("회원가입")
+                    .font(.finda(.heading4))
+                    .foregroundStyle(Color.Gray.gray80)
+                    .padding(.bottom, 64)
+
+                VStack(spacing: 32) {
+                    AuthTextField(
+                        type: .base,
+                        placeholder: "시크릿 키를 입력해주세요",
+                        label: "시크릿 키",
+                        text: $store.secretKeyText
+                    )
+                    FINDAButton(
+                        buttonText: "다음",
+                        isDisabled: store.signupButtonIsDisable,
+                        buttonClick: {}
+                    )
+                    AuthPromptButton(
+                        promptText: "계정이 있으신가요?",
+                        buttonText: "로그인",
+                        action: {}
+                    )
+                }
+                .padding(.horizontal, 24)
+
                 Spacer()
             }
-            .padding(.horizontal, 24)
-            .padding(.vertical, 8)
-
-            Text("회원가입")
-                .font(.finda(.heading4))
-                .foregroundStyle(Color.Gray.gray80)
-                .padding(.bottom, 64)
-
-            VStack(spacing: 32) {
-                AuthTextField(
-                    type: .base,
-                    placeholder: "시크릿 키를 입력해주세요",
-                    label: "시크릿 키",
-                    text: $secretKeyText
-                )
-                FINDAButton(
-                    buttonText: "다음",
-                    isDisabled: signupButtonIsDisable,
-                    buttonClick: {}
-                )
-                AuthPromptButton(
-                    promptText: "계정이 있으신가요?",
-                    buttonText: "로그인",
-                    action: {}
-                )
-            }
-            .padding(.horizontal, 24)
-
-            Spacer()
+            .dismissKeyboardOnTap()
+            .navigationBarBackButtonHidden(true)
+            .toolbar(.hidden, for: .navigationBar)
         }
-        .dismissKeyboardOnTap()
-        .navigationBarBackButtonHidden(true)
-        .toolbar(.hidden, for: .navigationBar)
     }
 }
 
 #Preview {
     SecretKeyView(
-        secretKeyText: .constant(""),
-        signupButtonIsDisable: true
+        store: Store(initialState: SecretKeyFeature.State()) {
+            SecretKeyFeature()
+        }
     )
 }
