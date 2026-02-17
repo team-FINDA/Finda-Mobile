035592aeea9ae403dc803887f368a3580c81179c
circle0802 <hawon9781@gmail.com>
2026-02-17 12:32:46 +0900

    ✨ :: (#13) ActivityTagCell 적용

--- Projects/Features/FeatureStudent/Sources/VolunteerList/VolunteerPost/VolunteerPostView.swift
+++ Projects/Features/FeatureStudent/Sources/VolunteerList/VolunteerPost/VolunteerPostView.swift
@@ -3,77 +3,84 @@ import DesignSystem
 
 struct VolunteerPostView: View {
     @Environment(\.dismiss) private var dismiss
+    private let activityTags = ["교장실", "상담실 분리수거", "제2 교무실", "제3 교무실", "청죽관 분리수거", "교실 분리수거"]
 
     var body: some View {
-        HStack {
-            Button(action: { dismiss() }, label: {
+        VStack {
+            HStack {
+                Button(action: { dismiss() }, label: {
+                    Image.Icons.leftArrow
+                        .foregroundStyle(Color.Gray.gray80)
+                })
+
+                Spacer()
+
+                Text("봉사활동 제목??")
+                    .font(.finda(.body1))
+                    .foregroundColor(.Gray.gray90)
+
+                Spacer()
+
                 Image.Icons.leftArrow
-                    .foregroundStyle(Color.Gray.gray80)
-            })
-
-            Spacer()
-
-            Text("봉사활동 제목??")
-                .font(.finda(.body1))
-                .foregroundColor(.Gray.gray90)
-
-            Spacer()
-
-            Image.Icons.leftArrow
-                .opacity(0)
-                .accessibilityHidden(true)
-        }
-        .padding(.horizontal, 24)
-        .padding(.vertical, 8)
-
-        VStack(spacing: 20) {
-            VStack(alignment: .leading, spacing: 5) {
-                Text("봉사활동 제목")
-                    .font(.finda(.heading5))
-                    .foregroundColor(.Gray.gray90)
-                Text("봉사활동 내용입니다.")
-                    .font(.finda(.body4))
-                    .foregroundColor(.Gray.gray90)
+                    .opacity(0)
+                    .accessibilityHidden(true)
             }
-            .frame(maxWidth: .infinity, alignment: .leading)
-            .padding(.top, 20)
+            .padding(.horizontal, 24)
+            .padding(.vertical, 8)
 
-            VStack(spacing: 15) {
-                HStack(spacing: 13) {
-                    InfoView(
-                        icon: Image.Icons.calendar,
-                        title: "신청 기간",
-                        content: "2025.3.10 ~\n2025.3.12",
-                        date: true
-                    )
-                    .frame(maxWidth: .infinity)
-                    InfoView(
-                        icon: Image.Icons.calendar,
-                        title: "활동 기간",
-                        content: "2025.3.10 ~\n2025.3.12",
-                        date: true
-                    )
-                    .frame(maxWidth: .infinity)
+            ScrollView {
+                VStack(spacing: 20) {
+                    VStack(alignment: .leading, spacing: 5) {
+                        Text("봉사활동 제목")
+                            .font(.finda(.heading5))
+                            .foregroundColor(.Gray.gray90)
+                        Text("봉사활동 내용입니다.")
+                            .font(.finda(.body4))
+                            .foregroundColor(.Gray.gray90)
+                    }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.top, 20)
+                    
+                    VStack(spacing: 15) {
+                        HStack(spacing: 13) {
+                            InfoView(
+                                icon: Image.Icons.calendar,
+                                title: "신청 기간",
+                                content: "2025.3.10 ~\n2025.3.12",
+                                date: true
+                            )
+                            .frame(maxWidth: .infinity)
+                            InfoView(
+                                icon: Image.Icons.calendar,
+                                title: "활동 기간",
+                                content: "2025.3.10 ~\n2025.3.12",
+                                date: true
+                            )
+                            .frame(maxWidth: .infinity)
+                        }
+                        HStack(spacing: 13) {
+                            InfoView(
+                                icon: Image.Icons.time,
+                                title: "봉사 기간",
+                                content: "8시간"
+                            )
+                            .frame(maxWidth: .infinity)
+                            InfoView(
+                                icon: Image.Icons.people,
+                                title: "활동 기간",
+                                content: "20명"
+                            )
+                            .frame(maxWidth: .infinity)
+                        }
+                    }
+                    
+                    ActivityTagSection(tags: activityTags)
+                    
+                    Spacer()
                 }
-                HStack(spacing: 13) {
-                    InfoView(
-                        icon: Image.Icons.time,
-                        title: "봉사 기간",
-                        content: "8시간"
-                    )
-                    .frame(maxWidth: .infinity)
-                    InfoView(
-                        icon: Image.Icons.people,
-                        title: "활동 기간",
-                        content: "20명"
-                    )
-                    .frame(maxWidth: .infinity)
-                }
+                .padding(.horizontal, 24)
             }
-
-            Spacer()
         }
-        .padding(.horizontal, 24)
     }
 }
 
