6e79cd7f7e831480c1b4c002cabc746ea98061bc
circle0802 <hawon9781@gmail.com>
2026-02-19 09:51:44 +0900

    ✨ :: (#15) ScheduleView Header 생성

--- Projects/Features/FeatureSchedule/Sources/ScheduleView.swift
+++ Projects/Features/FeatureSchedule/Sources/ScheduleView.swift
@@ -1,10 +1,21 @@
 import SwiftUI
+import DesignSystem
 
 public struct ScheduleView: View {
     public init() {}
 
     public var body: some View {
-        Text("Schedule")
+        VStack {
+            HStack {
+                Text("봉사활동 일정")
+                    .font(.finda(.body1))
+                    .foregroundColor(.Gray.gray90)
+            }
+            .padding(.horizontal, 24)
+            .padding(.vertical, 8)
+
+            Spacer()
+        }
     }
 }
 
