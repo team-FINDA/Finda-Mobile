8897e6240322ec8b000afe42f3db21c0e3ea3cc1
circle0802 <hawon9781@gmail.com>
2026-02-20 12:23:06 +0900

    ✨ :: (#17) ConfirmPopup Component 구현

---
+++ Projects/DesignSystem/Sources/Popup/ConfirmPopupView.swift
@@ -0,0 +1,60 @@
+import SwiftUI
+
+public struct ConfirmPopupView: View {
+    var title: String
+    var content: String
+    var action: () -> Void
+
+    public init(
+        title: String,
+        content: String,
+        action: @escaping () -> Void = {}
+    ) {
+        self.title = title
+        self.content = content
+        self.action = action
+    }
+
+    public var body: some View {
+        VStack(spacing: 15) {
+            VStack(spacing: 5) {
+                Image.Icons.check
+
+                Text(title)
+                    .font(.finda(.body3))
+                    .foregroundColor(.Gray.gray90)
+
+                Text(content)
+                    .font(.finda(.caption4))
+                    .foregroundColor(.Gray.gray80)
+            }
+
+            Button {
+                action()
+            } label: {
+                Text("확인")
+                    .font(.finda(.caption1))
+                    .frame(maxWidth: .infinity)
+                    .padding(.vertical, 10)
+                    .foregroundStyle(Color.Gray.gray10)
+            }
+            .background(Color.Blue.blue40)
+            .cornerRadius(32)
+        }
+        .padding(20)
+        .overlay(
+            RoundedRectangle(cornerRadius: 10)
+                .stroke(Color.Gray.gray80, lineWidth: 0.5)
+        )
+    }
+}
+
+#Preview {
+    VStack {
+        ConfirmPopupView(
+            title: "신청 완료!",
+            content: "봉사 활동 신청이 완료되었습니다.",
+            action: {}
+        )
+    }.padding(.horizontal, 64)
+}
