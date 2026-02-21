851e971ee1ad852358abf3fe7d39ada4619ad0ca
circle0802 <hawon9781@gmail.com>
2026-02-21 15:10:26 +0900

    ✨ :: (#21) history feature 생성 및 화면 이동

---
+++ Projects/Features/FeatureMy/Sources/VolunteerHistory/VolunteerHistoryFeature.swift
@@ -0,0 +1,27 @@
+import ComposableArchitecture
+import Shared
+
+@Reducer
+public struct VolunteerHistoryFeature {
+    @ObservableState
+    public struct State: Equatable {
+
+        public init() {}
+    }
+
+    public enum Action: BindableAction {
+        case binding(BindingAction<State>)
+    }
+
+    public init() {}
+
+    public var body: some ReducerOf<Self> {
+        BindingReducer()
+        Reduce { state, action in
+            switch action {
+            case .binding:
+                return .none
+            }
+        }
+    }
+}
