7e545b156ae2f56fca71b56fad1a5cbfb387515d
circle0802 <hawon9781@gmail.com>
2026-02-21 15:22:42 +0900

    ✨ :: (#21) SettingView 생성 및 header 구현

---
+++ Projects/Features/FeatureMy/Sources/Setting/SettingView.swift
@@ -0,0 +1,40 @@
+import SwiftUI
+import DesignSystem
+
+struct SettingView: View {
+    @Environment(\.dismiss) private var dismiss
+
+    var body: some View {
+        VStack(spacing: 20) {
+            HStack {
+                Button(action: { dismiss() }, label: {
+                    Image.Icons.leftArrow
+                        .foregroundStyle(Color.Gray.gray80)
+                })
+
+                Spacer()
+
+                Text("설정")
+                    .font(.finda(.body1))
+                    .foregroundColor(.Gray.gray90)
+
+                Spacer()
+
+                Image.Icons.leftArrow
+                    .opacity(0)
+                    .accessibilityHidden(true)
+            }
+            .padding(.horizontal, 24)
+            .padding(.vertical, 8)
+
+            Spacer()
+        }
+        .navigationBarBackButtonHidden(true)
+        .toolbar(.hidden, for: .navigationBar)
+        .toolbar(.hidden, for: .tabBar)
+    }
+}
+
+#Preview {
+    SettingView()
+}
