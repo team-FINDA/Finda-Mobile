f01c71707b5179cf78cb307f7f1f75d7bce836ed
circle0802 <hawon9781@gmail.com>
2026-02-23 18:53:02 +0900

    ✨ :: (#24) CustomToggle Component 구현

---
+++ Projects/Features/FeatureMy/Sources/Setting/AlertSetting/Component/CustomToggle.swift
@@ -0,0 +1,32 @@
+import SwiftUI
+import DesignSystem
+
+struct CustomToggle: View {
+    var title: String
+    var font: Font
+    @Binding var isOn: Bool
+
+    init(title: String, font: Font, isOn: Binding<Bool>) {
+        self.title = title
+        self.font = font
+        self._isOn = isOn
+    }
+
+    var body: some View {
+        HStack {
+            Text(title)
+                .font(font)
+                .foregroundStyle(Color.Gray.gray90)
+
+            Spacer()
+
+            Toggle("", isOn: $isOn)
+                .labelsHidden()
+                .toggleStyle(SwitchToggleStyle(tint: Color.Blue.blue30))
+        }
+    }
+}
+
+#Preview {
+    CustomToggle(title: "공지사항", font: .finda(.subheading2), isOn: .constant(false))
+}
