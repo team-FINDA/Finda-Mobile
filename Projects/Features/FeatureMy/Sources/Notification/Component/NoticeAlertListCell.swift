c83ccae9f33dee7bb2219ab411dc66f9b6762e58
circle0802 <hawon9781@gmail.com>
2026-02-24 10:51:08 +0900

    ✨ :: (#22) NotificationAlertListCell Component 구현

--- Projects/Features/FeatureMy/Sources/Notification/Component/NoticeAlertListCell.swift
+++ Projects/Features/FeatureMy/Sources/Notification/Component/NoticeAlertListCell.swift
@@ -5,38 +5,51 @@ struct NoticeAlertListCell: View {
     var title: String
     var date: String
     var time: String
+    var action: () -> Void
 
-    init(title: String, date: String, time: String) {
+    init(
+        title: String,
+        date: String,
+        time: String,
+        action: @escaping () -> Void = {}
+    ) {
         self.title = title
         self.date = date
         self.time = time
+        self.action = action
     }
 
     var body: some View {
-        HStack(spacing: 12) {
-            Image.Icons.notification
-                .padding(8)
-                .background(Color.Blue.blue10)
-                .clipShape(.rect(cornerRadius: 8))
+        Button(action: action) {
+            HStack(spacing: 12) {
+                Image.Icons.notification
+                    .padding(8)
+                    .background(Color.Blue.blue10)
+                    .clipShape(.rect(cornerRadius: 8))
 
-            VStack(alignment: .leading, spacing: 4) {
-                Text(title)
-                    .font(.finda(.body1))
-                    .foregroundStyle(Color.Gray.gray90)
+                VStack(alignment: .leading, spacing: 4) {
+                    Text(title)
+                        .font(.finda(.body1))
+                        .foregroundStyle(Color.Gray.gray90)
 
-                HStack(spacing: 8) {
-                    Text(date)
-                        .font(.finda(.body4))
-                        .foregroundStyle(Color.Gray.gray70)
+                    HStack(spacing: 8) {
+                        Text(date)
+                            .font(.finda(.body4))
+                            .foregroundStyle(Color.Gray.gray70)
 
-                    Text(time)
-                        .font(.finda(.body4))
-                        .foregroundStyle(Color.Gray.gray70)
+                        Text(time)
+                            .font(.finda(.body4))
+                            .foregroundStyle(Color.Gray.gray70)
+                    }
                 }
-            }
 
-            Spacer()
+                Spacer()
+            }
+            .padding(.horizontal, 4)
+            .padding(.vertical, 16)
+            .background(Color.Gray.gray10)
         }
+        .buttonStyle(.plain)
     }
 }
 
